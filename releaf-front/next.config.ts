f948b18d21e3d29ce4354780c63db66669b93fcb
유수현 <rhan0871@naver.com>
2025-11-30 22:15:23 +0900

    feat: 카카오 CDN 이미지 호스트명 추가

--- releaf-front/next.config.ts
+++ releaf-front/next.config.ts
@@ -20,6 +20,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "http",
+        hostname: "img1.kakaocdn.net",
+      },
+      {
+        protocol: "http",
         hostname: "k.kakaocdn.net",
       },
       {
