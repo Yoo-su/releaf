2768755562bde4053b0c7c60f3aa2d28f25fd245
유수현 <rhan0871@naver.com>
2025-12-20 14:49:54 +0900

    feat: 책 상세페이지 하단 관련 리뷰 스켈레톤 ui 수정

--- bookjeok-front/src/features/book/components/related-reviews/skeleton.tsx
+++ bookjeok-front/src/features/book/components/related-reviews/skeleton.tsx
@@ -1,32 +1,19 @@
-import { Skeleton } from "@/shared/components/shadcn/skeleton";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { ReviewCardSkeleton } from "@/features/review/components/review-card/skeleton";
 
 /**
  * 관련 리뷰 섹션 스켈레톤
+ * - 슬라이더 형태로 표시
  */
 export const RelatedReviewsSkeleton = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <Swiper spaceBetween={16} slidesPerView="auto" className="w-full">
       {[...Array(4)].map((_, i) => (
-        <div
-          key={i}
-          className="flex h-[180px] bg-white rounded-xl overflow-hidden border border-stone-100"
-        >
-          {/* 이미지 스켈레톤 */}
-          <Skeleton className="w-[120px] h-full shrink-0" />
-
-          {/* 콘텐츠 스켈레톤 */}
-          <div className="flex-1 flex flex-col p-3">
-            <Skeleton className="h-3 w-20 mb-1" />
-            <Skeleton className="h-3 w-16 mb-3" />
-            <Skeleton className="h-5 w-full mb-2" />
-            <Skeleton className="h-4 w-3/4 mb-auto" />
-            <div className="flex items-center gap-2 pt-2 border-t border-stone-100">
-              <Skeleton className="w-5 h-5 rounded-full" />
-              <Skeleton className="h-3 w-16" />
-            </div>
-          </div>
-        </div>
+        <SwiperSlide key={i} className="w-[320px]! sm:w-[380px]!">
+          <ReviewCardSkeleton />
+        </SwiperSlide>
       ))}
-    </div>
+    </Swiper>
   );
 };
